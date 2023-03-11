2bb63bb7c93abcb47ba04963f25f5b5249a78a50
RokuVengador <125324301+RokuVengador@users.noreply.github.com>
2023-03-10 22:53:08 -0600

    roku
    triangulo de navidad

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+Console.WriteLine("Bienvenido");
+Console.WriteLine("Ingresa un numero entero:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("      ===============        ");
+Console.WriteLine("============================");
+Console.WriteLine("   =======================");
+Console.WriteLine("=============================");
+Console.WriteLine("  =========================");
+Console.WriteLine("        ============");
+
+
+for (int i = 1; i <= n; i++)
+{
+    for (int roku = 1; roku <= i; roku++)
+    {
+       Console.Write("*");
+       
+    }
+       Console.WriteLine();
+   
+
+
+}
+Console.WriteLine(n);
+Console.WriteLine(n);
+Console.WriteLine(n);
+Console.WriteLine("Feliz navidad y prospero año nuevo.     ^^^^^^^^^");
